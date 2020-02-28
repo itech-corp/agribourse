2ad88277364e9f28450e4b3c5de4875bdf9c3e92
Itech <ibrahimmahamatmoussa@gmail.com>
2020-02-28 23:49:09 +0100

    Updates

---
+++ precache-manifest.44fc7a015daecf8334971103f7f0194f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eb3773b6c38ce7533826b9e823ee792",
+    "url": "/agribourse/index.html"
+  },
+  {
+    "revision": "c43450491c421952b553",
+    "url": "/agribourse/static/css/main.377ce2a7.chunk.css"
+  },
+  {
+    "revision": "ae9280269a162a48cada",
+    "url": "/agribourse/static/js/2.baf33c77.chunk.js"
+  },
+  {
+    "revision": "af8649c7c4566122fbcfde407c2bb15b",
+    "url": "/agribourse/static/js/2.baf33c77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c43450491c421952b553",
+    "url": "/agribourse/static/js/main.5d656e59.chunk.js"
+  },
+  {
+    "revision": "f8bf93e4c45d38e87f77",
+    "url": "/agribourse/static/js/runtime-main.f04ff16a.js"
+  },
+  {
+    "revision": "f0e92cc43e7483f100d8b4c663d44d80",
+    "url": "/agribourse/static/media/Agribourse2.f0e92cc4.png"
+  },
+  {
+    "revision": "e31fcf1885e371e19f5786c2bdfeae1b",
+    "url": "/agribourse/static/media/Roboto-Bold.e31fcf18.ttf"
+  },
+  {
+    "revision": "46e48ce0628835f68a7369d0254e4283",
+    "url": "/agribourse/static/media/Roboto-Light.46e48ce0.ttf"
+  },
+  {
+    "revision": "894a2ede85a483bf9bedefd4db45cdb9",
+    "url": "/agribourse/static/media/Roboto-Medium.894a2ede.ttf"
+  },
+  {
+    "revision": "df7b648ce5356ea1ebce435b3459fd60",
+    "url": "/agribourse/static/media/Roboto-Regular.df7b648c.ttf"
+  },
+  {
+    "revision": "94998475f6aea65f558494802416c1cf",
+    "url": "/agribourse/static/media/Roboto-Thin.94998475.ttf"
+  },
+  {
+    "revision": "fd44b07aaf120feee8adeda059faf6f0",
+    "url": "/agribourse/static/media/bg-cpu.fd44b07a.jpg"
+  },
+  {
+    "revision": "6121232083996c53a92f60af62fff972",
+    "url": "/agribourse/static/media/bg-pattern.61212320.svg"
+  },
+  {
+    "revision": "289c9c6a0b5f3a176eac3fd8791e6829",
+    "url": "/agribourse/static/media/fr.289c9c6a.png"
+  },
+  {
+    "revision": "d9b287c5a99a97695ce0ef2496ee55c9",
+    "url": "/agribourse/static/media/us.d9b287c5.png"
+  }
+]);
